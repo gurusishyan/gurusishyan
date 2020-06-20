4a9be5710d1b2c55ffb73f6bf6e0c32453a6e2f8
sathyanarayanan-v <svvsathyanarayanan@gmail.com>
2020-06-20 19:43:53 +0530

    TypeORM Basic Setup
    Why: The need for doing the commit was
    
    * Type ORM Basic setup
    
    This commit addresses the need by:
    
    * Typeorm module setup
    *  PostgreSQL compose file to run postgresql

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm'
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    "type":"postgres",
+    "host":process.env.DB_HOST,
+    "port":parseInt(process.env.DB_PORT,10),
+    "username":process.env.DB_USERNAME,
+    "password":process.env.DB_PASSWORD,
+    "database":process.env.DATABASE_NAME,
+    "synchronize":true,
+    "logging":true,
+    "entities":["./src/**/*.entity.ts","./dist/**/*.entity.js"]
+})],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,14 +7,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  await app.listen(port, '0.0.0.0',() => {
+    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix,'Bootstrap');
   });
 }
 
