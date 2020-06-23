1b936af2ca0a386051f3cdbaf9eb423b29c71332
sathyanarayanan-v <svvsathyanarayanan@gmail.com>
2020-06-23 20:06:59 +0530

    DTO and Validation  for User Module Setup Why: The need for doing the commit was
    *  Validation and DTO Setup
    
    This commit addresses the need by:
    
    * Validation for User Module with DTOs' setup done

--- apps/backend/src/app/user/user.dto.ts
+++ apps/backend/src/app/user/user.dto.ts
@@ -1,5 +1,16 @@
-export interface UserDTO{
+import {IsUUID,IsString,IsIn, IsNotEmpty} from 'class-validator'
+import { globalConfig } from '@gurusishyan-config';
+export class CreateUserDTO{
+    @IsUUID('all',{message:"Invalid Format: User ID"})
+    @IsNotEmpty()
     user_id:string;
+
+    @IsString({message:"ValidationError: User name is missing"})
+    @IsNotEmpty()
     user_name:string;
+
+    @IsString({message:"User role is missing"})
+    @IsIn(globalConfig.roles,{message:"invalid option: invalid user role"})
+    @IsNotEmpty()
     user_role:string
 }
