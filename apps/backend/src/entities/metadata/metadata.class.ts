acc46356e8667bfc528af6b9a7e58b5e2b83b47a
sathyanarayanan-v <svvsathyanarayanan@gmail.com>
2020-06-23 20:07:23 +0530

    DTO and Validation  for Metadata Setup Why: The need for doing the commit was
    *  Validation and DTO Setup
    
    This commit addresses the need by:
    
    * Validation for Metadata with DTOs' setup done

--- apps/backend/src/entities/metadata/metadata.class.ts
+++ apps/backend/src/entities/metadata/metadata.class.ts
@@ -1,11 +1,37 @@
-export class IMetadata {
-    class_of_study: string;
-    subject_interested: string;
-    board_of_education: string;
-    lesson_on_subject: string;
-    additional_notes: string;
-    document_attached: boolean;
-    document_url: string;
-    upvotes: number;
-    downvotes: number;
-  }
+import { IsString, IsPositive, IsOptional, IsBoolean,IsIn } from 'class-validator';
+import { globalConfig } from '@gurusishyan-config';
+export class MetadataDTO {
+  @IsString({ message: 'Class of study is mandatory and should be of type string' })
+  @IsIn(globalConfig.classes,{message:"Invalid Option: Class of Study"})
+  class_of_study: string;
+
+  @IsString({ message: 'Required Field Error: Subject Interested' })
+  subject_interested: string;
+
+  @IsString({ message: 'Required Field Error: Board Of Education' })
+  @IsIn(globalConfig.board_of_education,{message:"Invalid Option: Board Of Education"})
+  board_of_education: string;
+
+  @IsString({ message: 'Required Field Error: Lesson' })
+  @IsOptional()
+  lesson_on_subject: string;
+
+  @IsOptional()
+  @IsString({ message: 'Required Field Error: Board Of Education' })
+  additional_notes: string;
+
+  @IsBoolean({ message: 'Required Field Error: Document Attached' })
+  document_attached: boolean;
+
+  @IsOptional()
+  @IsString({ message: 'Required Field Error: Board Of Education' })
+  document_url: string;
+
+  @IsPositive({ message: 'ValidationError: Upvotes should be a positive number' })
+  @IsOptional()
+  upvotes: number;
+
+  @IsPositive({ message: 'ValidationError: Downvotes should be a positive number' })
+  @IsOptional()
+  downvotes: number;
+}
