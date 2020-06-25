a523ca7aacb1c161ad5c6caec2168e30e8180809
sathyanarayanan-v <svvsathyanarayanan@gmail.com>
2020-06-25 20:25:39 +0530

    Swagger Initial Setup Why: The need for doing the commit was
    * Swagger Module setup
    
    This commit addresses the need by:
    
    * Swagger registration for development build

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,21 +3,48 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import {loggerInstance} from '@gurusishyan-logger'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { loggerInstance } from '@gurusishyan-logger';
 import { AppModule } from './app/app.module';
+
+function bindSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('GuruSishyan')
+    .setDescription('To make education simple and free')
+    .setVersion('1.0')
+    .addTag('API Spec')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+}
+
 async function bootstrap() {
-  loggerInstance.log('Initializing Nest Module',"info")
+  loggerInstance.log('Initializing Nest Module', 'info');
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  const host = process.env.HOST || '0.0.0.0'
-  const protocol = process.env.API_PROTOCOL || 'http'
-  await app.listen(port, host,() => {
-    Logger.log(`Gurusishyan server running at ${protocol}://${host}:${port}/${globalPrefix}\n`,'BootstrapModule',false);
-    loggerInstance.log(`Nest App Initialized and successfully running at ${protocol}://${host}:${port}/${globalPrefix}`)
+  const host = process.env.HOST || '0.0.0.0';
+  const protocol = process.env.API_PROTOCOL || 'http';
+  if (process.env.NODE_ENV === 'development') {
+    bindSwagger(app);
+    Logger.log(
+      `Swagger running at ${protocol}://${host}:${port}/api-docs`,
+      'SwaggerModule'
+    );
+  }
+  await app.listen(port, host, () => {
+    Logger.log(
+      `Gurusishyan server running at ${protocol}://${host}:${port}/${globalPrefix}\n`,
+      'BootstrapModule',
+      false
+    );
+    loggerInstance.log(
+      `Nest App Initialized and successfully running at ${protocol}://${host}:${port}/${globalPrefix}`
+    );
   });
 }
 
