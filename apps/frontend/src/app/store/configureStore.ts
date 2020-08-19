038c3e201f6910d3e4f11162c1e9847eeeb1801c
prem2204 <prempranesh2204@gmail.com>
2020-08-19 09:16:37 +0530

    Added :: redux persist

--- apps/frontend/src/app/store/configureStore.ts
+++ apps/frontend/src/app/store/configureStore.ts
@@ -1,12 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk';
 
 import reducer from './root-reducer';
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(thunk))
-)
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-export default store
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+export let store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)))
+export let persistor = persistStore(store)
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './app/app';
-import store from './app/store/configureStore';
+import { store, persistor } from './app/store/configureStore';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
