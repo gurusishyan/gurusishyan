329544b50d9c615be3ecc7be5dd9b5a4549a0e8c
prem2204 <prempranesh2204@gmail.com>
2020-08-26 17:35:25 +0530

    Updated :: test cases

--- apps/frontend/src/app/auth/components/reset-password/reset-password.spec.tsx
+++ apps/frontend/src/app/auth/components/reset-password/reset-password.spec.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import ResetPassword from './reset-password';
+import { Provider } from 'react-redux';
+import { store } from '../../../store/configureStore';
 
 describe(' ResetPassword', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<ResetPassword />);
+    const { baseElement } = render(
+      <Provider store={store}>
+        {' '}
+        <ResetPassword />{' '}
+      </Provider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
